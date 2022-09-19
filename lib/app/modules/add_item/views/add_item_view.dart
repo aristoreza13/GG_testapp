21b3d154c1a5f9c12d1da2b2ee7dc0a8ade5c3bc
aristoreza13 <rezaaristo04@gmail.com>
2022-09-19 18:12:10 +0700

    Add Item page created

--- lib/app/modules/add_item/views/add_item_view.dart
+++ lib/app/modules/add_item/views/add_item_view.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:testapp/app/routes/app_pages.dart';
 
 import '../controllers/add_item_controller.dart';
 
@@ -13,11 +15,73 @@ class AddItemView extends GetView<AddItemController> {
         title: const Text('AddItemView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'AddItemView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: [
+          const TextField(
+            autocorrect: false,
+            // // controller: controller.nameC,
+            // maxLength: 16,
+            decoration: InputDecoration(
+              labelText: "ID",
+              icon: Icon(Icons.numbers),
+            ),
+          ),
+          const TextField(
+            autocorrect: false,
+            // controller: controller.nameC,
+            decoration: InputDecoration(
+              labelText: "Item",
+              icon: Icon(CupertinoIcons.cube),
+            ),
+          ),
+          const TextField(
+            autocorrect: false,
+            // controller: controller.nameC,
+            decoration: InputDecoration(
+              labelText: "Price",
+              icon: Icon(CupertinoIcons.money_dollar),
+            ),
+          ),
+          const TextField(
+            autocorrect: false,
+            // controller: controller.nameC,
+            decoration: InputDecoration(
+              labelText: "Description",
+              icon: Icon(Icons.web),
+            ),
+          ),
+          const TextField(
+            autocorrect: false,
+            // controller: controller.nameC,
+            decoration: InputDecoration(
+              labelText: "Category",
+              icon: Icon(Icons.list),
+            ),
+          ),
+          const TextField(
+            autocorrect: false,
+            // controller: controller.nameC,
+            decoration: InputDecoration(
+              labelText: "Image",
+              icon: Icon(Icons.image),
+            ),
+          ),
+          const TextField(
+            autocorrect: false,
+            // controller: controller.nameC,
+            decoration: InputDecoration(
+              labelText: "Rating",
+              icon: Icon(Icons.rate_review),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () => Get.offAllNamed(Routes.HOME),
+              child: const Text('Add Item')),
+        ],
       ),
     );
   }
